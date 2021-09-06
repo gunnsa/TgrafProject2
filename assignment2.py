57cf0492819ae1871632de65754946e21484a173
Isol <isolsig97@gmail.com>
2021-09-06 17:59:15 +0200

    fixed import for Mac

--- assignment2.py
+++ assignment2.py
@@ -1,7 +1,21 @@
 import pygame
 from pygame.locals import *
 
-from OpenGL.GL import *
+try:
+    try:
+        from OpenGL.GL import * # this fails in <=2020 versions of Python on OS X 11.x
+    except ImportError:
+        print('Drat, patching for Big Sur')
+        from ctypes import util
+        orig_util_find_library = util.find_library
+        def new_util_find_library( name ):
+            res = orig_util_find_library( name )
+            if res: return res
+            return '/System/Library/Frameworks/'+name+'.framework/'+name
+        util.find_library = new_util_find_library
+        from OpenGL.GL import *
+except ImportError:
+    pass
 from OpenGL.GLU import *
 
 
